40446d720d16a5b5507898d4dcd5100ee67dc54d
Helen Schwan <helenschwan3@gmail.com>
2020-09-24 14:56:03 +0100

    attempt to use nivo library

--- src/App.js
+++ src/App.js
@@ -1,35 +1,86 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { getTeams } from './services/FootballDataService';
+import { testData } from './services/FootballDataService';
+import { ResponsiveScatterPlot } from '@nivo/scatterplot';
 
 function App() {
-	const [teams, setTeams] = useState({});
+	//	const [teams, setTeams] = useState({});
+	//const [stats, setStats] = useState({});
+	const [filteredStats, setFilteredStats] = useState({});
 
 	useEffect(() => {
-		const teamLookup = getTeams();
-		setTeams(teamLookup);
+		//const teamLookup = getTeams();
+		//	const statsLookup = getStats();
+		const filteredresults = testData();
+		//	setTeams(teamLookup);
+		//	setStats(statsLookup);
+		setFilteredStats(filteredresults);
 	}, []);
 
-	console.log(Object.values(teams));
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				{Object.values(teams).map((t, index) => (
-					<p key={index}>{t.team_name}</p>
-				))}
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				{/* {Object.values(teams).map((t, index) => (
+					<p key={index}>{t.name}</p>
+				))} */}
+				<div className="container">
+					<ResponsiveScatterPlot
+						data={filteredStats}
+						margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
+						xScale={{ type: 'linear', min: 0, max: 'auto' }}
+						xFormat={function (e) {
+							return e + ' kg';
+						}}
+						yScale={{ type: 'linear', min: 0, max: 'auto' }}
+						yFormat={function (e) {
+							return e + ' cm';
+						}}
+						blendMode="multiply"
+						axisTop={null}
+						axisRight={null}
+						axisBottom={{
+							orient: 'bottom',
+							tickSize: 5,
+							tickPadding: 5,
+							tickRotation: 0,
+							legend: 'weight',
+							legendPosition: 'middle',
+							legendOffset: 46,
+						}}
+						axisLeft={{
+							orient: 'left',
+							tickSize: 5,
+							tickPadding: 5,
+							tickRotation: 0,
+							legend: 'size',
+							legendPosition: 'middle',
+							legendOffset: -60,
+						}}
+						legends={[
+							{
+								anchor: 'bottom-right',
+								direction: 'column',
+								justify: false,
+								translateX: 130,
+								translateY: 0,
+								itemWidth: 100,
+								itemHeight: 12,
+								itemsSpacing: 5,
+								itemDirection: 'left-to-right',
+								symbolSize: 12,
+								symbolShape: 'circle',
+								effects: [
+									{
+										on: 'hover',
+										style: {
+											itemOpacity: 1,
+										},
+									},
+								],
+							},
+						]}
+					/>
+				</div>
 			</header>
 		</div>
 	);
--- src/services/FootballDataService.js
+++ src/services/FootballDataService.js
@@ -2,6 +2,7 @@ import { Matches } from './Data/MatchData';
 import { Players } from './Data/PlayerData';
 import { Stats } from './Data/StatsData';
 import { Teams } from './Data/TeamData';
+import { TestData } from './Data/TestData';
 
 export const getMatches = () => {
 	return Matches;
@@ -17,4 +18,33 @@ export const getStats = () => {
 
 export const getTeams = () => {
 	return Teams;
+};
+
+export const filterStats = () => {
+	let filteredStats = {};
+
+	// for (const g of Stats) {
+	// 	//console.log(g);
+	// 	filteredStats[g.player_id] = {
+	// 		left: g.left_foot_passes,
+	// 		right: g.right_foot_passes,
+	// 	};
+	// }
+
+	filteredStats.id = 'Group A';
+	filteredStats.data = [];
+
+	for (const g of Stats) {
+		//console.log(g);
+		filteredStats.data.push({
+			x: g.left_foot_passes,
+			y: g.right_foot_passes,
+		});
+	}
+
+	return filteredStats;
+};
+
+export const testData = () => {
+	return TestData;
 };
