563b19d9482d29f091cec0f6c90c7c1b0eb18042
Helen Schwan <helenschwan3@gmail.com>
2020-09-25 16:49:07 +0100

    Get chart filtering by country (#3)
    * attempt to use nivo library
    
    * graphs actually rendering
    
    * graph rendering football data
    
    * data analysis to make easy to chart
    
    * removing additional logging
    
    * working filtering graph

--- src/App.js
+++ src/App.js
@@ -1,35 +1,136 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { getTeams } from './services/FootballDataService';
+import {
+	mapTeams,
+	filterStats,
+	getCountries,
+} from './services/FootballDataService';
+import { ResponsiveScatterPlot } from '@nivo/scatterplot';
 
 function App() {
-	const [teams, setTeams] = useState({});
+	const [filteredStats, setFilteredStats] = useState({});
+	const [originalStats, setOriginalStats] = useState({});
+	const [playersByTeam, setPlayersByTeam] = useState({});
+	const [currentTeam, setCurrentTeam] = useState('all');
+	const [countryList, setCountryList] = useState({});
 
 	useEffect(() => {
-		const teamLookup = getTeams();
-		setTeams(teamLookup);
+		//const footballData = filterStats();
+		const countryData = getCountries();
+		setCountryList(countryData);
+		const data = mapTeams();
+		setPlayersByTeam(data);
+		//	console.log(footballData);
+
+		const footballData = filterStats(data);
+		setFilteredStats(footballData);
+		setOriginalStats(footballData);
 	}, []);
 
-	console.log(Object.values(teams));
+	const handleFilterTeam = (e) => {
+		if (e.target.value === 'all') {
+			setFilteredStats(originalStats);
+			setCurrentTeam(e.target.value);
+			return;
+		}
+		let newChartData = [];
+		setCurrentTeam(e.target.value);
+		let newStats = originalStats.find((f) => f.id === e.target.value);
+		newChartData.push(newStats);
+		setFilteredStats(newChartData);
+	};
+
+	const resetGraph = (e) => {
+		setCurrentTeam('all');
+		setFilteredStats(originalStats);
+	};
+
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				{Object.values(teams).map((t, index) => (
-					<p key={index}>{t.team_name}</p>
-				))}
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				<div className="container">
+					<select
+						name="country"
+						onChange={(e) => handleFilterTeam(e)}
+						onBlur={(e) => handleFilterTeam(e)}
+						value={currentTeam}
+					>
+						{' '}
+						<option key="all" value="all">
+							Please select a team
+						</option>
+						{Object.values(countryList).map((c, key) => {
+							return (
+								<option key={key} value={c}>
+									{c}
+								</option>
+							);
+						})}
+					</select>
+					<button
+						className="form-spacing"
+						onClick={(e) => resetGraph(e)}
+					>
+						Reset graph
+					</button>
+					<ResponsiveScatterPlot
+						data={filteredStats}
+						margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
+						xScale={{ type: 'linear', min: 0, max: 'auto' }}
+						xFormat={function (e) {
+							return e + ' kg';
+						}}
+						yScale={{ type: 'linear', min: 0, max: 'auto' }}
+						yFormat={function (e) {
+							return e + ' cm';
+						}}
+						colors={{ scheme: 'set3' }}
+						blendMode="multiply"
+						axisTop={null}
+						axisRight={null}
+						axisBottom={{
+							orient: 'bottom',
+							tickSize: 5,
+							tickPadding: 5,
+							tickRotation: 0,
+							legend: 'left-footedness(%)',
+							legendPosition: 'middle',
+							legendOffset: 46,
+						}}
+						axisLeft={{
+							orient: 'left',
+							tickSize: 5,
+							tickPadding: 5,
+							tickRotation: 0,
+							legend: 'right-footness(%)',
+							legendPosition: 'middle',
+							legendOffset: -60,
+						}}
+						legends={[
+							{
+								anchor: 'bottom-right',
+								direction: 'column',
+								justify: false,
+								translateX: 130,
+								translateY: 0,
+								itemWidth: 100,
+								itemHeight: 12,
+								itemsSpacing: 5,
+								itemDirection: 'left-to-right',
+								symbolSize: 12,
+								symbolShape: 'circle',
+								effects: [
+									{
+										on: 'hover',
+										style: {
+											itemOpacity: 1,
+										},
+									},
+								],
+							},
+						]}
+					/>
+				</div>
 			</header>
 		</div>
 	);
--- src/services/FootballDataService.js
+++ src/services/FootballDataService.js
@@ -2,6 +2,7 @@ import { Matches } from './Data/MatchData';
 import { Players } from './Data/PlayerData';
 import { Stats } from './Data/StatsData';
 import { Teams } from './Data/TeamData';
+import { TestData } from './Data/TestData';
 
 export const getMatches = () => {
 	return Matches;
@@ -17,4 +18,183 @@ export const getStats = () => {
 
 export const getTeams = () => {
 	return Teams;
+};
+
+// export const filterStats = () => {
+// 	let filteredStats = [];
+
+// 	let currentStats = {};
+// 	currentStats['id'] = 'Group A';
+// 	currentStats['data'] = [];
+
+// 	for (const g of Stats) {
+// 		currentStats.data.push({
+// 			x: g.left_foot_passes,
+// 			y: g.right_foot_passes,
+// 		});
+// 	}
+// 	filteredStats.push(currentStats);
+// 	return filteredStats;
+// };
+
+// export const testData = () => {
+// 	return TestData;
+// };
+
+export const getCountries = () => {
+	let countryList = [];
+	Teams.map((t) => {
+		return countryList.push(t.team_name);
+	});
+	return countryList;
+};
+
+export const filterStats = (playersByTeam) => {
+	let filteredStats = [];
+	for (let p in playersByTeam) {
+		let currentStats = {};
+		currentStats['id'] = playersByTeam[p].name;
+		currentStats['data'] = [];
+		for (let player in playersByTeam[p].players) {
+			//console.log(playersByTeam[p].players);
+			// if (playersByTeam[p].winPercentage > 0) {
+			// 	currentStats.data.push({
+			// 		x: playersByTeam[p].players[player].leftFootPasses,
+			// 		y: playersByTeam[p].players[player].rightFootPasses,
+			// 	});
+			// }
+			let currentPlayer = playersByTeam[p].players[player];
+			currentStats.data.push({
+				x: (currentPlayer.leftFootPasses / currentPlayer.passes) * 100,
+				y: (currentPlayer.rightFootPasses / currentPlayer.passes) * 100,
+			});
+		}
+		//console.log(playersByTeam[p]);
+		filteredStats.push(currentStats);
+	}
+	console.log(filteredStats);
+	return filteredStats;
+};
+
+export const mapTeams = () => {
+	let playersByTeam = [];
+	let players,
+		stats = null;
+	Teams.map((t) => {
+		let mappedMatches = [];
+		let currentTeam = {};
+		players = filterPlayersByCountry(t.team_name);
+		stats = filterStatsByCountry(t.team_id);
+		let matches = filterMatchesByCountry(t.team_id);
+
+		matches.map((m) => {
+			let mappedMatch = processMatchData(m, t.team_id);
+			mappedMatches.push(mappedMatch);
+			return mappedMatches;
+		});
+		let winPercentage = calculateWinPercentage(mappedMatches);
+		let mappedPlayer = handledPlayersMapping(players, stats);
+		currentTeam = mapCountry(t, mappedPlayer, mappedMatches, winPercentage);
+		playersByTeam.push(currentTeam);
+		return playersByTeam;
+	});
+	return playersByTeam;
+};
+
+const filterPlayersByCountry = (currentCountry) => {
+	return Players.filter((p) => p.country_name === currentCountry);
+};
+
+const filterStatsByCountry = (countryId) => {
+	return Stats.filter((s) => s.team_id === countryId);
+};
+
+const filterMatchesByCountry = (countryId) => {
+	return Matches.filter(
+		(m) =>
+			m.match_home_team_id === countryId ||
+			m.match_away_team_id === countryId
+	);
+};
+
+const processMatchData = (match, countryId) => {
+	let isHomeTeam = match.match_home_team_id === countryId;
+
+	let result = checkScore(match.match_home_score, match.match_away_score);
+	if (result === null) {
+		result = checkScore(
+			match.match_home_penalty_score,
+			match.match_away_penalty_score
+		);
+	}
+	let matchResult = {
+		isHomeTeam: isHomeTeam,
+		result: (isHomeTeam && result) || (!isHomeTeam && !result) ? 'W' : 'L',
+	};
+
+	return matchResult;
+};
+
+const calculateWinPercentage = (mappedMatches) => {
+	let matchCount = mappedMatches.length;
+	let noOfWins = mappedMatches.filter((m) => m.result === 'W').length;
+	return (noOfWins / matchCount) * 100;
+};
+
+const checkScore = (homeScore, awayScore) => {
+	let result = null;
+	if (homeScore !== awayScore) {
+		return homeScore > awayScore;
+	}
+	return result;
+};
+
+const handledPlayersMapping = (players, stats) => {
+	let mappedPlayers = [];
+	for (let s in stats) {
+		let matchingPlayer = filterPlayersByPlayerId(
+			players,
+			stats[s].player_id
+		);
+		let currentPlayer = mapPlayer(stats[s], matchingPlayer[0]);
+		mappedPlayers.push(currentPlayer);
+	}
+	return mappedPlayers;
+};
+
+const filterPlayersByPlayerId = (players, statvalue) => {
+	return players.filter((p) => p.player_id === statvalue);
+};
+
+const mapPlayer = (stat, player) => {
+	return {
+		name: player.player_known_name
+			? player.player_known_name
+			: player.player_name,
+		id: player.player_id,
+		matchId: stat.match_id,
+		minutesPlayed: stat.minutes_played,
+		teamPossessionPercentage: stat.team_possession_percentage,
+		expectedGoals: stat.xg,
+		shots: stat.shots,
+		goals: stat.goals,
+		tackles: stat.tackles,
+		interceptions: stat.interceptions,
+		pressures: stat.pressures,
+		passes: stat.passes,
+		completedPasses: stat.completed_passes,
+		leftFootPasses: stat.left_foot_passes,
+		rightFootPasses: stat.right_foot_passes,
+		shotsFaced: stat.player_shots_faced,
+	};
+};
+const mapCountry = (team, players, matches, winPercentage) => {
+	let currentTeam = {};
+	currentTeam['name'] = team.team_name;
+	currentTeam['id'] = team.team_id;
+	currentTeam['colour'] = team.team_first_color;
+	currentTeam['players'] = players;
+	currentTeam['matches'] = matches;
+	currentTeam['winPercentage'] = winPercentage;
+	return currentTeam;
 };
